9902990ebb9e5b313446298b399d7884d2c3b427
Hanna <annlisandias@gmail.com>
2020-11-04 01:58:04 +0100

    Практика 2 - перед проверкой 2

---
+++ 2.js
@@ -0,0 +1,156 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(leftBorderWidth);
+
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+// console.log(name);
+// var name = 'Anna';
+
+// {
+//     var result = 50;
+// }
+
+// console.log(result);
+
+// alert(5);
+// [].push('a');
+
+
+
+// let number = 4.6;
+
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const person = '5';
+
+// const bool = false;
+
+// console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+// console.log(obj["name"]);
+
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp', 8, {}, []];
+
+// console.log(arr[1]);
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt('Вам есть 18?', '18');
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', "");
+// answers[1] = prompt('Как ваша фамилия?', "");
+// answers[2] = prompt('Сколько вам лет?', "");
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+// alert(`Привет, ${user}`);
+
+// console.log('arr' + ' - object');
+// console.log(4 + '5');
+
+// let incr = 10,
+//     decr = 10;
+
+// // ++incr;
+// // --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// // постфиксная форма сначала увеличивает или уменьшает старое значение, а только потом начинает работу
+
+// console.log(5%2);
+
+// console.log(2 + 2 * 2 !== '6');
+
+// const isChecked = false,
+//       isClose = false;
+
+// console.log(isChecked || !isClose);
+
+// if (4) {
+//     console.log('Ok!');
+// } else {
+//     console.log('Error');
+// }
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Слишком много');
+// } else {
+//     console.log('Ok');
+// }
+
+// (num === 50) ? console.log('Ok') : console.log('Error');
+
+// const num = 50;
+
+// switch (num) {
+//     case 49:
+//         console.log('Неверно');
+//         break;
+//     case 100:
+//         console.log('Неверно');
+//         break;
+//     case 50:
+//         console.log('Верно');
+//         break;
+//     default:
+//         console.log('Не в этот раз');
+//         break;
+// }
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+let num = 50;
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        // break;
+        continue;
+    }
+    console.log(i);
+}
