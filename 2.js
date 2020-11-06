6b736e6375d37d3cbf1296769827b47dafe6a948
Hanna <annlisandias@gmail.com>
2020-11-07 00:11:07 +0100

    Функции

--- 2.js
+++ 2.js
@@ -145,12 +145,50 @@
 // }
 // while (num < 55);
 
-let num = 50;
+// let num = 50;
 
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        // break;
-        continue;
-    }
-    console.log(i);
-}
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// let num = 20;
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//     let num = 10;
+//     console.log(num);
+// }
+
+// showFirstMessage('Hello World!');
+// console.log(num);
+
+// function calc(a, b) {
+//     return (a + b);
+// }
+
+// console.log(calc(4, 3));
+
+// ret();
+
+// function ret() {
+//     let num = 50;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+
+// const logger = function() {
+//     console.log('Hello');
+// };
+
+// logger();
+
+const calc = (a, b) => { return a + b };
+
+
+
