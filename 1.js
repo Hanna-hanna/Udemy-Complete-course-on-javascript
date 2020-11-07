0f11059845ae6181855cea888a62ff3de4101c03
Hanna <annlisandias@gmail.com>
2020-11-07 17:29:22 +0100

    Практика 3

--- 1.js
+++ 1.js
@@ -2,7 +2,15 @@
 
 //Практика 1
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -12,28 +20,49 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ""),
-          b = +prompt('На сколько оцените его?', "");
-    if (a === '' || b === '' || a.length > 50 || a === 0 || b === 0) {
-        console.log('sd');
-        i--;
-    } else {
-        personalMovieDB.movies[a] = b;
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`, '');
     }
 }
 
-if (personalMovieDB.count < 10) {
-    alert("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    alert("Вы классический зритель");
-} else if (personalMovieDB.count > 30) {
-    alert("Вы киноман");
-} else {
-    alert("Произошла ошибка");
+writeYourGenres();
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ""),
+              b = +prompt('На сколько оцените его?', "");
+        if (a === '' || b === '' || a.length > 50 || a === null || b === null) {
+            i--;
+        } else {
+            personalMovieDB.movies[a] = b;
+        }
+    }
 }
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        alert("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        alert("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        alert("Вы киноман");
+    } else {
+        alert("Произошла ошибка");
+    }
+}
+
+detectPersonalLevel();
 
 // personalMovieDB.movies[a] = b; // как записать ключ и значение в объект!!!лучше не через точку
 // personalMovieDB.movies[c] = d; 
 
-console.log(personalMovieDB);
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
--- 2.js
+++ 2.js
@@ -188,7 +188,35 @@
 
 // logger();
 
-const calc = (a, b) => { return a + b };
+// const calc = (a, b) => { return a + b; };
+
+const str = 'TeSt';
+const arr = [1, 2, 4];
+
+//console.log(str[2] = 'd');
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
+
+const fruit = 'Some fruit';
+console.log(fruit.indexOf('g'));
+
+const logg = "Hello world";
+
+//console.log(logg.slice(6, 11)); // в аргументе end указываем то, что не включается
+//console.log(logg.slice(-5, -1)); 
+
+//console.log(logg.substring(6, 11)); 
+//console.log(logg.substr(6, 11)); // вместо аргумента end мы указываем длину, которую надо вырезать
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+//console.log(parseInt(test));
+console.log(parseFloat(test)); // возвращает значение с точкой
+
 
 
 
